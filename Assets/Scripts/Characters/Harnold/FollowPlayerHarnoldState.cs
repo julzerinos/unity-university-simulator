4f4df777bb73d08cfcf5f1844fb9531184e36cee
Julian Szachowicz <julian.szachowicz@gmail.com>
2022-01-24 21:29:25 +0100

    Imrove Harnold & add ECTS model

--- Assets/Scripts/Characters/Harnold/FollowPlayerHarnoldState.cs
+++ Assets/Scripts/Characters/Harnold/FollowPlayerHarnoldState.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Utils;
 
 namespace Characters.Harnold
 {
@@ -11,53 +12,73 @@ namespace Characters.Harnold
 
         private Vector3 _lastDirection = Vector3.zero;
 
-        private float checkDistance = 1f;
+        private LayerMask _mask = LayerMask.GetMask("Door");
+
+        private readonly float _checkDistance = 1f;
+
+        private void DirectionalCheck(ref Vector3 updateDirection, Vector3 direction, Vector3 counter)
+        {
+            Debug.DrawRay(_controller.transform.position, direction, Color.blue, 0f, false);
+
+            if (Physics.Raycast(_controller.transform.position, direction, out var check, _checkDistance, ~_mask))
+                updateDirection += (_checkDistance - check.distance) * counter;
+        }
 
         public override void FixedUpdate()
         {
-            // var brakePull = Vector3.zero;
+            // var momentum = _controller.transform.forward;
+            // DirectionalCheck(ref momentum, _controller.transform.position + _controller.transform.forward,
+            //     _controller.transform.position - _controller.transform.forward);
+            //
             // if (Physics.Raycast(_controller.transform.position,
             //     (_controller.transform.position + _controller.transform.forward).normalized, out var forwardCheck,
-            //     checkDistance))
-            //     brakePull += (checkDistance - forwardCheck.distance) * -_controller.transform.forward;
+            //     3))
+            // {
+            //     Debug.Log("hit");
+            //     momentum = -(checkDistance - forwardCheck.distance) * _controller.transform.forward;
+            // }
 
             var right45 = (_controller.transform.forward + _controller.transform.right).normalized;
             var right30 = (2 * _controller.transform.forward + _controller.transform.right).normalized;
+            var right60 = (_controller.transform.forward + 2 * _controller.transform.right).normalized;
             var right90 = _controller.transform.right;
-            var rightPull = Vector3.zero;
-            if (Physics.Raycast(_controller.transform.position, right45, out var leftCheck45, checkDistance))
-                rightPull += (checkDistance - leftCheck45.distance) * -_controller.transform.right;
-            if (Physics.Raycast(_controller.transform.position, right30, out var leftCheck30, checkDistance))
-                rightPull += (checkDistance - leftCheck30.distance) * -_controller.transform.right;
-            if (Physics.Raycast(_controller.transform.position, right90, out var leftCheck90, checkDistance))
-                rightPull += (checkDistance - leftCheck90.distance) * -_controller.transform.right;
 
             var left45 = (_controller.transform.forward - _controller.transform.right).normalized;
             var left30 = (2 * _controller.transform.forward - _controller.transform.right).normalized;
+            var left60 = (_controller.transform.forward - 2 * _controller.transform.right).normalized;
             var left90 = -_controller.transform.right;
+
+            var rightPull = Vector3.zero;
+            DirectionalCheck(ref rightPull, right45, left45);
+            DirectionalCheck(ref rightPull, right30, left60);
+            DirectionalCheck(ref rightPull, right90, _controller.transform.forward);
+
             var leftPull = Vector3.zero;
-            if (Physics.Raycast(_controller.transform.position, left45, out var rightCheck45, checkDistance))
-                leftPull += (checkDistance - rightCheck45.distance) * _controller.transform.right;
-            if (Physics.Raycast(_controller.transform.position, left30, out var rightCheck30, checkDistance))
-                leftPull += (checkDistance - rightCheck30.distance) * _controller.transform.right;
-            if (Physics.Raycast(_controller.transform.position, left90, out var rightCheck90, checkDistance))
-                leftPull += (checkDistance - rightCheck90.distance) * _controller.transform.right;
+            DirectionalCheck(ref leftPull, left45, right45);
+            DirectionalCheck(ref leftPull, left30, right60);
+            DirectionalCheck(ref leftPull, left90, _controller.transform.forward);
+
+            var playerDirection =
+                (_playerTransform.position.ToX0Z() - _controller.transform.position.ToX0Z()).normalized;
 
             var momentum = _controller.transform.forward;
-            var playerDirection = (_playerTransform.position - _controller.transform.position).normalized;
-            var weightedDirection = playerDirection + rightPull + leftPull + momentum;// + brakePull;
-            _lastDirection = Vector3.Lerp(_lastDirection, weightedDirection, .05f);
-            _controller.transform.LookAt(_controller.transform.position + _lastDirection, Vector3.up);
-            _controller.MoveForward();
 
-            // Debug.DrawLine(_controller.transform.position, _controller.transform.position + brakePull, Color.red, .0f,
-                // false);
-            Debug.DrawLine(_controller.transform.position, _controller.transform.position + leftPull, Color.blue, .0f,
+            Debug.DrawLine(_controller.transform.position, _controller.transform.position + playerDirection,
+                Color.magenta, .0f, false);
+            Debug.DrawLine(_controller.transform.position, _controller.transform.position + leftPull, Color.red, .0f,
                 false);
             Debug.DrawLine(_controller.transform.position, _controller.transform.position + rightPull, Color.green, .0f,
                 false);
-            Debug.DrawLine(_controller.transform.position, _controller.transform.position + playerDirection,
-                Color.magenta, .0f, false);
+
+            var weightedDirection = playerDirection + rightPull.ToX0Z() + leftPull.ToX0Z() + momentum.ToX0Z();
+
+            Debug.DrawLine(_controller.transform.position.ToX0Z(), _controller.transform.position + weightedDirection,
+                Color.black, .0f,
+                false);
+
+            _lastDirection = Vector3.Lerp(_lastDirection, weightedDirection, .05f);
+            _controller.transform.LookAt(_controller.transform.position + _lastDirection, Vector3.up);
+            _controller.MoveForward();
         }
     }
 }
